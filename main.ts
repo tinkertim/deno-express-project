9810bad07119f1bcb49321a70259d46bfb75183c
Tim Post <tinkertim@gmail.com>
2023-03-08 14:17:41 -0500

    Can be implicit when dealing with express types

--- main.ts
+++ main.ts
@@ -3,28 +3,25 @@ import express, { NextFunction, Request, Response } from "npm:express@4.18.2";
 import demoData from "./data_blob.json" assert { type: "json" };
 
 const app = express();
-const port: number = Number(Deno.env.get("APP_PORT")) || 3000;
+const port = Number(Deno.env.get("PORT")) || 3000;
 
-const reqLogger = function (req: Request, _res: Response, next: NextFunction) {
+const reqLogger = function (req, _res, next) {
   console.info(`${req.method} request to "${req.url}" by ${req.hostname}`);
   next();
 };
 
 app.use(reqLogger);
-app.get("/", (_req: Request, res: Response): void => {
-  res.status(200).send("Hello from Deno and Express!");
+
+app.get("/users", (_req, res) => {
+  res.status(200).json(demoData.users);
 });
 
-app.get("/users", (_req: Request, res: Response): void => {
-  res.send(demoData.users);
-});
-
-app.get("/users/:id", (req: Request, res: Response): void => {
+app.get("/users/:id", (req, res) => {
   const idx = Number(req.params.id);
   for (const needle in demoData.users) {
     const haystack = demoData.users[needle];
     if (haystack.id === idx) {
-      res.status(200).send(haystack);
+      res.status(200).json(haystack);
     }
   }
   res.status(400).json({ msg: "User not found" });
