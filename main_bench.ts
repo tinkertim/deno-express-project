717717696c089ca65e5bcce0bb507327fc8bf6b0
Tim Post <tinkertim@gmail.com>
2023-03-16 10:02:19 -0400

    Lint

--- main_bench.ts
+++ main_bench.ts
@@ -1,9 +1,9 @@
 const stagingUrl = Deno.env.get("STAGING_URL") || "http://localhost:3000/";
 
 Deno.bench(function requestUser() {
-    new URL(`${stagingUrl}/users/2`);
+  new URL(`${stagingUrl}/users/2`);
 });
 
 Deno.bench(function requestAllUsers() {
-    new URL(`${stagingUrl}/users`);
+  new URL(`${stagingUrl}/users`);
 });
